2fdcffeae8424c79a7349cd249710782becf4c5a
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-04-24 09:52:37 -0400

    fix: fix repo, switch to read stream

--- actions/discord-embed/src/discord-embed.ts
+++ actions/discord-embed/src/discord-embed.ts
@@ -2,9 +2,12 @@ import * as core from "@actions/core";
 import { HttpClient } from "@actions/http-client";
 import { Octokit } from "@octokit/action";
 import FormData from "form-data";
+import { createReadStream } from "fs";
+import { writeFile } from "fs/promises";
 
 enum Inputs {
-    WebhookUrls = "webhook-urls"
+    WebhookUrls = "webhook-urls",
+    Repo = "repo"
 }
 
 interface InputTypes {
@@ -19,13 +22,20 @@ declare global {
     }
 }
 
-const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
+const [owner, thisRepo] = process.env.GITHUB_REPOSITORY.split("/");
+console.log(
+    "🚀 ~ file: discord-embed.ts:25 ~ owner, thisRepo:",
+    owner,
+    thisRepo
+);
 
 async function execute() {
     const client = new HttpClient();
     const octokit = new Octokit();
     const webhook = core.getInput(Inputs.WebhookUrls, { required: true });
     console.log("🚀 ~ file: discord-embed.ts:28 ~ webhooks:", webhook);
+    const repo = core.getInput(Inputs.Repo) ?? thisRepo;
+    console.log("🚀 ~ file: discord-embed.ts:36 ~ repo:", repo);
     const urls = JSON.parse(`[]`).flat() as string[];
 
     /** get last release from github api */
@@ -38,14 +48,18 @@ async function execute() {
     );
 
     const body = release.data.body_html;
-    if (!body) core.error("No release found");
+    console.log("🚀 ~ file: discord-embed.ts:41 ~ body:", body);
+    if (!body) {
+        core.error("No release found");
+        return;
+    }
+    await writeFile("./body.html", body);
 
     /* for (const webhook of urls) { */
     console.log("🚀 ~ file: discord-embed.ts:44 ~ urls:", urls);
     /*  const response = await client.postJson(webhook, payload); */
     const formData = new FormData();
-    formData.append("upload-file", body);
-    formData.append("payload_json", JSON.stringify({}));
+    formData.append("upload-file", createReadStream("./body.html"));
     formData.submit(webhook, function (error, response) {
         if (error != null) {
             core.error(`failed to upload file: ${error.message}`);
