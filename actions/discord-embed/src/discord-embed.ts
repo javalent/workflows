b694d950094f26e17393a76616d90652a8fc9a13
Jeremy Valentine <38669521+valentine195@users.noreply.github.com>
2023-04-24 10:12:03 -0400

    remove logs

--- actions/discord-embed/src/discord-embed.ts
+++ actions/discord-embed/src/discord-embed.ts
@@ -19,20 +19,13 @@ declare global {
 }
 
 const [owner, thisRepo] = process.env.GITHUB_REPOSITORY.split("/");
-console.log(
-    "🚀 ~ file: discord-embed.ts:25 ~ owner, thisRepo:",
-    owner,
-    thisRepo
-);
 
 async function execute() {
     const client = new HttpClient();
     const octokit = new Octokit();
     const webhook = core.getInput(Inputs.WebhookUrls, { required: true });
-    console.log("🚀 ~ file: discord-embed.ts:28 ~ webhooks:", webhook);
     const repo = core.getInput(Inputs.Repo) ?? thisRepo;
     const name = core.getInput(Inputs.Name) ?? repo;
-    console.log("🚀 ~ file: discord-embed.ts:36 ~ repo:", repo);
     const urls = JSON.parse(`[]`).flat() as string[];
 
     /** get last release from github api */
@@ -52,14 +45,14 @@ async function execute() {
     }
 
     /* for (const webhook of urls) { */
-    console.log("🚀 ~ file: discord-embed.ts:44 ~ urls:", urls);
     const embeds = DME.render(body);
     for (const embed of embeds) {
+        console.log("🚀 ~ file: discord-embed.ts:58 ~ embed:", embed);
         embed.title = `${name} Release - ${version}`;
         embed.timestamp = release.data.published_at;
-        embed.auther = "Jeremy Valentine";
+        embed.author = "Jeremy Valentine";
     }
-    const response = await client.postJson(webhook, {
+    await client.postJson(webhook, {
         embeds
     });
 
